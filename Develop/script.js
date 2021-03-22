c5f77ad0f32a0b30b0cb0368c1f6a7413d24afb0
Joëlle Munzinger <jcmunzinger@gmail.com>
2021-03-22 09:42:54 +0800

    some changes

--- Develop/script.js
+++ Develop/script.js
@@ -7,12 +7,39 @@ var lowerCaseCharsChoice = "";
 var upperCaseCharsChoice = "";
 var numCharsChoice = "";
 var passwordChars = "";
+var passwordChars= "";
+var passwordLength= "";
 
 
 var upperCaseChars = "ABCDEFGHJKLMNPQRSTWXYZ";
 var lowerCaseChars = "abcdefgijkmnopqrstwxyz";
 var numChars = "0123456789";
 var specialChars = "*$-+?_&=!%{}/";
+
+//me: function to determine which character types the user wants to include in his password
+function getCharsforPassword () {
+  var lowerCaseCharsChoice = confirm("Would you like to include lowercase letters ?") ;
+    if (lowerCaseCharsChoice==true) {
+      passwordChars+=lowerCaseChars;
+}
+
+  var upperCaseCharsChoice = confirm("Would you like to include Uppercase letters ?") ;
+    if (upperCaseCharsChoice==true) {
+      passwordChars+=upperCaseChars;
+}
+
+
+  var numCharsChoice = confirm("Would you like to include numbers ?") ;
+    if (numCharsChoice ==true) {
+      passwordChars+=numChars;
+}
+
+  var specialCharsChoice = confirm ("Do you also want to include special characters ?") ;
+    if (specialCharsChoice ==true) { 
+      passwordChars+=specialChars;
+}
+  return passwordChars
+      }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
@@ -32,9 +59,8 @@ function writePassword() {
 
 // me: prompting user for password values and assigning them to a variable 
 function generatePassword() {
-  var passwordChars;
-  var passwordLength;
-  var 
+ 
+  
   
  
   passwordLength = parseInt(prompt("Password length: Enter a number between 8 and 128"));
@@ -49,48 +75,27 @@ function generatePassword() {
 
     if (passwordLength >=8) {
 
-      getCharsforPassword () ; 
+      getCharsForPassword ()
+      
 
     }
   // me: for loop to determine whether the user has entered at least one character type and if not then he will be asked the questions again 
-    if (lowerCaseCharsChoice==false && upperCaseCharsChoice==false && !numCharsChoice==false) {
-      alert ("You must at least use one character type in your password") ;
+    // if (lowerCaseCharsChoice==false && upperCaseCharsChoice==false && !numCharsChoice==false) {
+    //   alert ("You must at least use one character type in your password") ;
 
-      getCharsforPassword () ;
+    //   getCharsforPassword () ;
 }
   // me: for loop to generate random password 
 
   for (var i = 0; i < passwordLength; i++) {
     
-    randomPasswordGenerate += [Math.floor(Math.random() * passwordChars.length)]
+    randomPasswordGenerate += passwordChars[Math.floor(Math.random() * passwordChars.length)]
   }
-  return randomPasswordGenerate;
-    }
+  // return randomPasswordGenerate;
+    
     
      
 
 // me: function to input user's choices into a variable called passwordChars and return the result which will then be used in the for loop
 
-  function getCharsforPassword () {
-      var lowerCaseCharsChoice = confirm("Would you like to include lowercase letters ?") ;
-        if (lowerCaseCharsChoice==true) {
-          passwordChars+=lowerCaseChars;
-    }
-    
-      var upperCaseCharsChoice = confirm("Would you like to include Uppercase letters ?") ;
-        if (upperCaseCharsChoice==true) {
-          passwordChars+=upperCaseChars;
-    }
   
-
-      var numCharsChoice = confirm("Would you like to include numbers ?") ;
-        if (numCharsChoice ==true) {
-          passwordChars+=numChars;
-    }
-    
-      var specialCharsChoice = confirm ("Do you also want to include special characters ?") ;
-        if (specialCharsChoice ==true) { 
-          passwordChars+=specialChars;
-    }
-      return passwordChars
-          }
